1a10fb86b55c349e34487d83b2d491b250070c36
Ali Aljaffer <ahjfr@icloud.com>
2025-10-07 13:53:04 +0300

    Fixed moving files for sonarqube

--- terraform-sonarqube/azure/vm/cloud-init/sonarqube-install.sh
+++ terraform-sonarqube/azure/vm/cloud-init/sonarqube-install.sh
@@ -9,7 +9,8 @@ sudo useradd -m -d /opt/sonarqube sonarqube
 cd /opt
 sudo wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-10.2.1.78527.zip
 sudo unzip sonarqube-10.2.1.78527.zip
-sudo mv sonarqube-10.2.1.78527/ sonarqube
+sudo mv sonarqube-10.2.1.78527/* sonarqube/
+sudo rm -rf sonarqube-10.2.1.78527/
 sudo chown -R sonarqube:sonarqube /opt/sonarqube
 
 sudo -u sonarqube bash -c "/opt/sonarqube/bin/linux-x86-64/sonar.sh start"
