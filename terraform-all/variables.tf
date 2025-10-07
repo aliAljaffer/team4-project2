81bf8678409ebb4dc3e0a77a23d35031cebd0f3c
Ali Aljaffer <ahjfr@icloud.com>
2025-10-07 13:32:49 +0300

    Added curly brace to fix syntax error (sorry for merge to main)

--- terraform-all/variables.tf
+++ terraform-all/variables.tf
@@ -17,113 +17,113 @@ variable "location" {
   default = "ukwest"
 }
 variable "virtual_machine_name" {
- type = string
+  type = string
 }
 
 variable "disksize" {
- type    = string
- default = "Standard_DS1_v2"
+  type    = string
+  default = "Standard_DS1_v2"
 }
 variable "os_disk_name" {
 
- type = string
- }
- variable "os_disk_caching" {
+  type = string
+}
+variable "os_disk_caching" {
 
- type = string
- default = "ReadWrite"
- }
+  type    = string
+  default = "ReadWrite"
+}
 
 variable "os_disk_storage_account_type" {
 
- type = string 
- default = "Standard_LRS"
+  type    = string
+  default = "Standard_LRS"
 }
 
 variable "source_image_reference_publisher" {
 
- type = string
+  type = string
 
- default = "Canonical"
- }
+  default = "Canonical"
+}
 
 variable "source_image_reference_offer" {
 
- type = string
+  type = string
 
- default = "UbuntuServer"
- }
+  default = "UbuntuServer"
+}
 
 variable "source_image_reference_sku" {
 
- type = string
+  type = string
 
- default = "22.04-LTS"
- }
+  default = "22.04-LTS"
+}
 
 variable "source_image_reference_version" {
 
- type = string
+  type = string
 
- default =  "latest"
- }
+  default = "latest"
+}
 variable "vm_admin_username" {
 
- type = string
+  type = string
 
- default = "azureuser"
- }
+  default = "azureuser"
+}
 
 variable "vm_disable_password_authentication" {
 
- type = bool
+  type = bool
 
- default = false
- }
+  default = false
+}
 variable "vm_admin_password" {
-  type      = string
-
+  type = string
+}
 variable "fe_sunet_id" {
-  type    = string
-  
+  type = string
+
 }
 
 variable "be_sunet_id" {
-  type    = string
-  
+  type = string
+
 }
 
 variable "app_gw_ip" {
-  type    = string
-  
+  type = string
+
 }
 
 variable "db_host" {
-  type    = string
-  
+  type = string
+
 }
 
 variable "db_name" {
-  type    = string
-  
+  type = string
+
 }
 
 variable "db_port" {
-  type    = string
-  
+  type = string
+
 }
 
 variable "db_username" {
-  type    = string
-  
+  type = string
+
 }
 
 variable "db_password" {
-  type    = string
-  
+  type = string
+
 }
 
 variable "db_driver" {
-  type    = string
-  
+  type = string
+
 }
